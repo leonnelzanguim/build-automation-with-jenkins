9b58eca017c6d52227a86d81a40b7c0911b0993d
leonnel Zanguim <58272671+leonnelzanguim@users.noreply.github.com>
2025-09-29 12:53:30 +0200

    Update script.groovy

--- java-maven-app/script.groovy
+++ java-maven-app/script.groovy
@@ -1,14 +1,19 @@
-def buildApp() {
+def buildJar() {
   echo 'building the application...'
+  sh 'mvn package'
 }
 
-def testApp() {
+def buildImage() {
   echo 'testing the application...'
+  withCredentials([usernamePassword(credentialsId: 'docker-hub-repo', passwordVariable: 'PASS', usernameVariable: 'USER')]) {
+    sh 'docker build -t 10012975/demo-java-maven-app:2.0 -f java-maven-app/Dockerfile java-maven-app/'
+    sh 'echo $PASS | docker login -u $USER --password-stdin'
+    sh 'docker push 10012975/demo-java-maven-app:2.0'
+  }
 }
 
 def deployApp() {
   echo 'deploying the application...'
-  echo "deploying version ${params.VERSION}"
 }
 
 return this
