409689fbb850cbcd21c51a17553f7505fa0c4aaa
leonnel Zanguim <58272671+leonnelzanguim@users.noreply.github.com>
2025-09-29 16:05:19 +0200

    Update script.groovy

--- java-maven-app/script.groovy
+++ java-maven-app/script.groovy
@@ -1,16 +1,16 @@
-def buildJar() {
-  echo 'building the application...'
-  sh 'mvn -f java-maven-app/pom.xml package'
-}
+// def buildJar() {
+//   echo 'building the application...'
+//   sh 'mvn -f java-maven-app/pom.xml package'
+// }
 
-def buildImage() {
-  echo 'testing the application...'
-  withCredentials([usernamePassword(credentialsId: 'docker-hub-repo', passwordVariable: 'PASS', usernameVariable: 'USER')]) {
-    sh 'docker build -t 10012975/demo-java-maven-app:2.0 -f java-maven-app/Dockerfile java-maven-app/'
-    sh 'echo $PASS | docker login -u $USER --password-stdin'
-    sh 'docker push 10012975/demo-java-maven-app:2.0'
-  }
-}
+// def buildImage() {
+//   echo 'testing the application...'
+//   withCredentials([usernamePassword(credentialsId: 'docker-hub-repo', passwordVariable: 'PASS', usernameVariable: 'USER')]) {
+//     sh 'docker build -t 10012975/demo-java-maven-app:2.0 -f java-maven-app/Dockerfile java-maven-app/'
+//     sh 'echo $PASS | docker login -u $USER --password-stdin'
+//     sh 'docker push 10012975/demo-java-maven-app:2.0'
+//   }
+// }
 
 def deployApp() {
   echo 'deploying the application...'
